21cfe8678951e6ea9404cd856512c03cfc35c463
cjg <coolcjg@naver.com>
2025-11-07 20:06:42 +0900

    기본 구조

---
+++ src/main/java/com/cjg/clob/ClobApplication.java
@@ -0,0 +1,13 @@
+package com.cjg.clob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClobApplication.class, args);
+	}
+
+}
